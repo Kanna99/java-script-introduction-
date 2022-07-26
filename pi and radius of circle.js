1d8d04d42b41e94ede5fad8efab739fae14f6aa5
aparnachoudary <109720369+Kanna99@users.noreply.github.com>
2022-07-26 14:05:08 +0530

    Add files via upload

---
+++ pi and radius of circle.js
@@ -0,0 +1,30 @@
+class circle
+{
+    public static void main(string args[])
+{
+double pi=3.14,r=1,area;
+area=pi*r*r;
+system.out.println("area of circle is--");
+system.out.print(area);
+}
+}
+
+
+
+
+import java.util.scanner;
+class program1c
+{
+    public static void main(string args[])
+    {
+        scanner sc=new scanner(system.in);
+        {
+            double r;
+            system.out.print("enter the radius:-");
+            r = sc.nextDouble();
+            double area = Math.PI*r*r;
+            system.out.println("area of circle="+area);
+        }
+    }
+}
+
